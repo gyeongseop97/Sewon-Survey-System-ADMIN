35c8bc7a8f3481e4fcf212c4cafefa2563cd3ce6
gyeongseop97 <gyeongseop97@gmail.com>
2026-02-09 10:03:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -11,9 +11,7 @@
   // ------------------ Supabase init ------------------
 const SUPABASE_URL = "https://pztlmyfutfmbmlvavwuz.supabase.co";
 const SUPABASE_KEY = "sb_publishable_fnGFEvCmhZRRIWj0qrEEeA_Vex3mxac";
-
-// UMD로 로드하면 window.supabase가 생깁니다.
-const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
   async function sbSignInWithEmail(email, password) {
   const { data, error } = await sb.auth.signInWithPassword({ email, password });
